9f45ae77ff59e3b92dfb1b7367288497f88922d7
Zhihao Xu <zxu8@crcfe02.crc.nd.edu>
2024-10-02 10:46:34 -0400

    enhanced performance

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qalo",  # Replace with your package name
+    version="1.1.0",  # Initial version number
+    author="Zhihao Xu",  # Replace with your name
+    author_email="zxu8@nd.edu",  # Replace with your email
+    packages=find_packages(),  # Automatically find and include all packages in the directory
+)
